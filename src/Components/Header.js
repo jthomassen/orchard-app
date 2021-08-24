e903e84c2600421798b08c0caf36d22fc40b13a4
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-08-23 18:21:11 -0600

    added json data

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,16 @@ import React from 'react';
 function Header() {
     return (
         <div>
-            Header
+            <h1>Orchard</h1>
+            <form className="location-search">
+                <label className="input-box">
+                    <input 
+                        type="text" 
+                        name="name" 
+                        placeholder="Where do you want to live?"
+                        />
+                </label>
+            </form>
         </div>
     ); 
 }
--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import MoreInfo from './MoreInfo'
 
-function Post() {
+function Post({post}) {
+
+    const {id, title, photo_1, photo_2, photo_3, photo_4, photo_5, location, availability, size, price, pets} = post
+
     return (
-        <MoreInfo />
+        <div className="post">
+            <div className="image-container">
+                <img src={photo_1} alt={photo_1}/>
+                <img src={photo_2} alt={photo_2}/>
+                <img src={photo_3} alt={photo_3}/>
+                <img src={photo_4} alt={photo_4}/>
+                <img src={photo_5} alt={photo_5}/>
+            </div>
+            <div className="post-info">
+                <h3>{title}</h3>
+                <h4>{location}</h4>
+                <h4>{price}</h4>
+                <h5>{availability}</h5>
+                <h5>{size}</h5>
+                <h5>Allows Pets: {pets}</h5>
+            </div>
+            <MoreInfo />
+        </div>
     );
 }
 
